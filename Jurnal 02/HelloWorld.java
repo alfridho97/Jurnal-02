095d0c86d9bda88abfb4a8c8e98cb077e34ad9fd
alfridho97 <alfridho97@gmail.com>
2021-09-29 10:48:23 +0700

    HelloWorld.java

---
+++ Jurnal 02/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Alfridho!");
+		System.out.println("NIM: D3IF6706213131");
+		System.out.println("Kelas: 45-01");
+	}
+}
